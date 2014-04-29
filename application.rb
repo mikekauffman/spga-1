3312fcfbc448766df06f462417c9f12637078482
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-28 18:38:36 -0600

    dogs can be edited and removed from the database

--- application.rb
+++ application.rb
@@ -17,7 +17,7 @@ class Application < Sinatra::Application
     erb :new
   end
 
-  post '/dogs' do
+  post '/dogs/create' do
     @dogs_table.insert(
       breed: params[:dog_breed],
       color: params[:dog_color],
@@ -29,6 +29,25 @@ class Application < Sinatra::Application
   get '/dogs' do
     all_dogs = @dogs_table.to_a
     erb :dogs, locals: {all_dogs: all_dogs}
+  end
+
+  get '/dogs/:id' do
+    dog = @dogs_table[id: params[:id]]
+    erb :edit, locals: {dog: dog}
+  end
+
+  post '/dogs/edit/:id' do
+    @dogs_table.where(id: params[:id]).update(
+      breed: params[:dog_breed],
+      color: params[:dog_color],
+      size: params[:dog_size]
+    )
+    redirect '/dogs'
+  end
+
+  get '/dogs/delete/:id' do
+    @dogs_table.where(id: params[:id]).delete
+    redirect '/dogs'
   end
 
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -4,7 +4,7 @@ require 'capybara/rspec'
 Capybara.app = Application
 
 feature 'Homepage' do
-  scenario 'Dogs can be added to the database' do
+  scenario 'Dogs can be added to the database, edited and deleted' do
     visit '/'
 
     expect(page).to have_content 'Welcome!'
@@ -18,5 +18,37 @@ feature 'Homepage' do
     expect(page).to have_content 'Rottweiler'
     expect(page).to have_content 'Black and Tan'
     expect(page).to have_content 'Large'
+
+    visit '/'
+
+    click_on 'Add a Dog'
+
+    fill_in 'dog_breed', with: 'Vizsla'
+    fill_in 'dog_color', with: 'Cinnamon'
+    fill_in 'dog_size', with: 'Medium/Large'
+    click_on 'Submit'
+
+    click_on 'view Vizsla'
+
+    fill_in 'dog_breed', with: 'Vizsla'
+    fill_in 'dog_color', with: 'Cinnamon'
+    fill_in 'dog_size', with: 'Medium'
+    click_on 'Submit'
+
+    expect(page).to have_content 'Rottweiler'
+    expect(page).to have_content 'Black and Tan'
+    expect(page).to have_content 'Large'
+    expect(page).to have_content 'Vizsla'
+    expect(page).to have_content 'Cinnamon'
+    expect(page).to have_content 'Medium'
+
+    click_on 'view Vizsla'
+    click_on 'delete Vizsla'
+
+    expect(page).to have_content 'Rottweiler'
+    expect(page).to have_content 'Black and Tan'
+    expect(page).to have_content 'Large'
+    expect(page).to_not have_content 'Vizsla'
+
   end
 end
